fd69926d73fd185cd1ec383b1a43209d1db26ec1
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2024-03-15 16:26:42 -0700

    used HashSet to store unique elements for solving the Contains Duplicate problem

---
+++ Array & Hashing/containsDuplicate.cs
@@ -0,0 +1,16 @@
+public class Solution{
+    public bool ContainsDuplicate(int[] nums){
+        HashSet<int> set = new HashSet<int>();
+        foreach(int x in nums){
+            if(set.Contains(x)) return true;
+            set.Add(x);
+        }
+        return false;
+    }
+}
+
+//Time Comlexity:O(n) n is the number of elements in the input array nums
+    // and use foreach loop iterates through each elemen in the array
+
+//Space Complexity:O(n) n is the number of elements in the input array nums
+    // use HashSet to store unique elements from the array
